698e0495c1aa207db7444a6c3b6d0b486c70ab83
Olav Hermansen <olavzat@gmail.com>
2013-11-19 19:47:51 +0100

    Added functionality to control map by voice recognition, this is awesome.

--- OculusMundus/Form1.Designer.cs
+++ OculusMundus/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.webBrowser = new System.Windows.Forms.WebBrowser();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtSpeechOutput = new System.Windows.Forms.TextBox();
+            this.lblLocation = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,11 +62,41 @@
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.txtSpeechOutput);
+            this.panel1.Controls.Add(this.lblLocation);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(800, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(184, 452);
             this.panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Enable Speech";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtSpeechOutput
+            // 
+            this.txtSpeechOutput.Location = new System.Drawing.Point(9, 155);
+            this.txtSpeechOutput.Multiline = true;
+            this.txtSpeechOutput.Name = "txtSpeechOutput";
+            this.txtSpeechOutput.Size = new System.Drawing.Size(161, 162);
+            this.txtSpeechOutput.TabIndex = 2;
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(6, 55);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(46, 17);
+            this.lblLocation.TabIndex = 1;
+            this.lblLocation.Text = "label1";
             // 
             // Form1
             // 
@@ -76,6 +109,7 @@
             this.Text = "OculusMundus";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -85,6 +119,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.WebBrowser webBrowser;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox txtSpeechOutput;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- OculusMundus/Form1.cs
+++ OculusMundus/Form1.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,13 +17,28 @@ namespace OculusMundus
     [System.Runtime.InteropServices.ComVisibleAttribute(true)]
     public partial class Form1 : Form
     {
+        /* Speech Recognition */
+        private SpeechSynthesizer sSynth;
+        private PromptBuilder pBuilder;
+        private SpeechRecognitionEngine sRecognize;
+        private Boolean isSpeechRunning = false;
+
         public Form1()
         {
             InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
+
+            sSynth = new SpeechSynthesizer();
+            pBuilder = new PromptBuilder();
+            sRecognize = new SpeechRecognitionEngine(new CultureInfo("en-GB"));
+
+            button2.Enabled = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            /* Setup webbrowser */
             webBrowser.AllowWebBrowserDrop = false;
             webBrowser.IsWebBrowserContextMenuEnabled = false;
             webBrowser.WebBrowserShortcutsEnabled = false;
@@ -31,16 +50,77 @@ namespace OculusMundus
         }
 
 
-        /* Called from JavaScript */
-        public void Test(String message)
-        {
-            MessageBox.Show(message, "AWESOME");
-        }
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
             object[] name = { "Hi to you to!" };
             webBrowser.Document.InvokeScript("helloWorld", name);
         }
+
+        /* Enable speech recognition */
+        public void EnableSpeechRecognition()
+        {
+            Choices sList = new Choices();
+            sList.Add(new string[] { "zoom in", "zoom out", "center"});
+            Grammar gr = new Grammar(new GrammarBuilder(sList));
+            try
+            {
+                sRecognize.RequestRecognizerUpdate();
+                sRecognize.LoadGrammar(gr);
+                sRecognize.SpeechRecognized += sRecognise_SpeechRecognized;
+                sRecognize.SetInputToDefaultAudioDevice();
+                sRecognize.RecognizeAsync(RecognizeMode.Multiple);
+                sRecognize.Recognize();
+
+            }
+
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+
+        private void sRecognise_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            txtSpeechOutput.AppendText(" " + e.Result.Text.ToString());
+            object[] command = { e.Result.Text.ToString() };
+            webBrowser.Document.InvokeScript("zoomHandler", command);
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isSpeechRunning) {
+                button2.Text = "Enable Speech";
+                sRecognize.RecognizeAsyncCancel();
+                isSpeechRunning = false;
+            }
+            else
+            {
+                button2.Text = "Disable Speech";
+                EnableSpeechRecognition();
+                isSpeechRunning = true;
+            }
+        
+        }
+
+        /* Called from JavaScript */
+        public void LocationUpdate(double lat, double lon)
+        {
+            lblLocation.Text = "Lat: " + lat + Environment.NewLine + " Lon: " + lon;
+        }
+
+        public void EarthLoaded()
+        {
+            button2.Enabled = true;
+        }
+
+        public void SendVersion(String version)
+        {
+            Form1.ActiveForm.Text = "OculusMondus - Google Earth - " + version;
+        }
+
     }
 }
